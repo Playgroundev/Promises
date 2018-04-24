c7f96a32533e792de685f2cc6f5f9145d65278e7
mtotodev05 <mtotodev05@gmail.com>
2018-04-24 21:45:25 +0300

    Emma6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var isMomHappy = false;
+var isMomHappy = true;
 
 var willIGetNewPhone = new Promise(
     function(resolve,reject){
@@ -8,7 +8,7 @@ var willIGetNewPhone = new Promise(
               brand: 'Samsung',
               color: 'black'
             };
-            resolve(reason);
+
             resolve(phone);
         }else{
             var reason = new Error('mom is not happy');
@@ -16,21 +16,49 @@ var willIGetNewPhone = new Promise(
         }
     }
 );
-
 var askMom = function(){
-    willIGetNewPhone.then(function(fulfilled){
-       console.log(fulfilled);
+    console.log('before asking mom')
+    willIGetNewPhone.then(showOff).then(function(fulfilled){
+        console.log(fulfilled);
     })
         .catch(function(error){
             console.log(error.message);
         });
 };
+
+
+var showOff = function(phone){
+    return new Promise(
+        function(resolve,reject){
+            var message = 'Hey Friend, I have a new ' + phone.color + ' ' + phone.brand + ' phone';
+            resolve(message);
+        }
+    );
+};
+/*The Below Code Describes the task in EmmaScript Version 6*/
+const isMomHapp = false;
+
+const getNewPhone = new Promise(
+    (resolve,reject) =>{
+        if(isMomHapp){
+            const phon = {
+                brand: 'Samsung',
+                color: 'Black',
+            };
+            resolve(phon);
+        }else{
+            const reaso = new Error('Mom is not happy');
+            reject(reaso);
+        }
+    }
+);
+const showOf = function(phon){
+    const message = "Hey Friend, I have a new " + phon.color + ' ' + phon.brand + ' phone';
+    return Promise.resolve(message);
+};
+
+const askMo = function(){
+    getNewPhone.then(showOf).then(fulfilled => console.log(fulfilled)).catch(error => console.log(error.message));
+}
 askMom();
-
-
-
-
-
-
-
-
+askMo();
